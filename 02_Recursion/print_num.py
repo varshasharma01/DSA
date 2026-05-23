de7e94a86ab7effcf9d3d57f7bfaf43dc941d294
varshasharma01 <varshasinbox1@gmail.com>
2026-05-23 09:18:05 +0530

    tail recursion

--- 02_Recursion/print_num.py
+++ 02_Recursion/print_num.py
@@ -1,6 +1,6 @@
 # let's say we want to print number from 1 to N  there are two methods one is head and another is tail
 
-# using tail recursion
+# using head recursion
 
 def print_num(i,n):
     
@@ -13,3 +13,14 @@ def print_num(i,n):
     
 print(print_num(1,5))
         
+        
+# using tail recursion
+
+def rev_num(n):
+    if n ==0:
+        return
+    else:
+        rev_num(n-1)
+        print(n, end=" ")
+        
+print(rev_num(5))
